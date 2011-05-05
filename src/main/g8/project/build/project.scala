6e18ef333c2d3d3bf834f0cba074d73541724b6a
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-05-05 12:21:34 +0200

    Upgraded to Scala 2.9.0.RC3.

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -10,8 +10,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
 
   // Test dependencies
   val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
-  val scalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.9-SNAPSHOT" % "test"
-  val specs2 = "org.specs2" %% "specs2" % "1.2" % "test"
+//  val scalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.9-SNAPSHOT" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.3-SNAPSHOT" % "test"
 
   // Test frameworks
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
